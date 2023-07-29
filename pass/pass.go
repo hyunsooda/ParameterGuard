eb824e14396fa2f28d3a18719c1134dc8ecfc224
Hyunsoo Shin <hyunsooda@kaist.ac.kr>
2023-07-29 23:05:08 +0900

    [Pass] Implemented struct access and dereference violation for N depth of them

--- pass/pass.go
+++ pass/pass.go
@@ -1,7 +1,6 @@
 package pass
 
 import (
-	"fmt"
 	"go/ast"
 	"go/token"
 	"go/types"
@@ -11,15 +10,9 @@ import (
 	"golang.org/x/tools/go/ast/inspector"
 )
 
-type ParamUsage struct {
-	Param      *types.Var
-	GuardAt    ast.Node
-	UseAt      ast.Node
-	DeclaredAt token.Pos
-}
-
 var (
-	TestOn   = false
+	Testing Test
+
 	Analyzer = &analysis.Analyzer{
 		Doc:      "Perform static analysis on Go source files to identify unsafe practices, such as nil dereferences, using a heuristic-based approach.",
 		Name:     "unsafeuse",
@@ -28,18 +21,111 @@ var (
 	}
 )
 
-func NewParamUsage(param *types.Var, GuardAt, UseAt ast.Node, DeclaredAt token.Pos) *ParamUsage {
-	return &ParamUsage{
-		Param:      param,
-		GuardAt:    GuardAt,
-		UseAt:      UseAt,
-		DeclaredAt: DeclaredAt,
+func aggregateNamedTyps(pass *analysis.Pass) map[types.Object]*ast.StructType {
+	insp := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
+	filterNodes := []ast.Node{
+		(*ast.GenDecl)(nil),
 	}
+	typObjs := make(map[types.Object]*ast.StructType)
+	insp.Preorder(filterNodes, func(n ast.Node) {
+		if genDecl, ok := n.(*ast.GenDecl); ok {
+			if len(genDecl.Specs) > 0 {
+				// TODO: loop the Sepcs slice
+				if typSpec, ok := genDecl.Specs[0].(*ast.TypeSpec); ok {
+					if structTyp, ok := typSpec.Type.(*ast.StructType); ok {
+						obj := pass.TypesInfo.Defs[typSpec.Name]
+						typObjs[obj] = structTyp
+					}
+				}
+			}
+		}
+	})
+	return typObjs
+}
+
+func getStructTyp(typObjs map[types.Object]*ast.StructType, targetTyp types.Type) *ast.StructType {
+	for typ, structTyp := range typObjs {
+		if unwrapPtrTyp(typ.Type()) == unwrapPtrTyp(targetTyp) {
+			return structTyp
+		}
+	}
+	return nil
+}
+
+func unwrapPtrTyp(typ types.Type) types.Type {
+	if t, ok := typ.(*types.Pointer); ok {
+		return unwrapPtrTyp(t.Elem())
+	}
+	return typ
+}
+
+func mapMerge(m1, m2 NamedTypes) NamedTypes {
+	m := make(map[types.Type]map[*ast.Ident]types.Object)
+	for declTyp, structTyp := range m1 {
+		m[declTyp] = make(map[*ast.Ident]types.Object)
+		for ident, obj := range structTyp {
+			m[declTyp][ident] = obj
+		}
+	}
+	for declTyp, structTyp := range m2 {
+		m[declTyp] = make(map[*ast.Ident]types.Object)
+		for ident, obj := range structTyp {
+			m[declTyp][ident] = obj
+		}
+	}
+	return m
+}
+
+func isCollected(collected NamedTypes, target types.Object) bool {
+	for _, structTyp := range collected {
+		for _, obj := range structTyp {
+			if obj == target {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+/// getAllInnerTyps recursively collect struct inner types
+func getAllInnerTyps(pass *analysis.Pass, collected NamedTypes, params []types.Object, namedTyps map[types.Object]*ast.StructType) map[types.Type]map[*ast.Ident]types.Object {
+	m := make(NamedTypes)
+
+	for _, param := range params {
+		for declTyp, structTyp := range namedTyps {
+			objTyp := declTyp.Type()
+			_, ok := objTyp.(*types.Named)
+			if !ok { // Not a struct type
+				continue
+			}
+			if unwrapPtrTyp(param.Type()) != objTyp {
+				continue
+			}
+			m[objTyp] = make(map[*ast.Ident]types.Object)
+			for _, targetField := range structTyp.Fields.List {
+				for _, targetFieldNm := range targetField.Names {
+					fieldObj := pass.TypesInfo.Defs[targetFieldNm]
+					m[objTyp][targetFieldNm] = fieldObj
+
+					// self-reference structure create infinite loop
+					if isCollected(collected, fieldObj) {
+						continue
+					}
+
+					for _, innerTyps := range getAllInnerTyps(pass, mapMerge(m, collected), []types.Object{fieldObj}, namedTyps) {
+						for innerNm, innerT := range innerTyps {
+							m[objTyp][innerNm] = innerT
+						}
+					}
+				}
+			}
+		}
+	}
+	return m
 }
 
 func run(pass *analysis.Pass) (interface{}, error) {
 	config := parseConfig(pass)
-
 	insp := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
 	filterNodes := []ast.Node{
 		(*ast.FuncDecl)(nil),
@@ -49,17 +135,21 @@ func run(pass *analysis.Pass) (interface{}, error) {
 			if !isInExcludes(pass, fnDecl, config) {
 				if fn, ok := pass.TypesInfo.Defs[fnDecl.Name]; ok {
 					sig := fn.Type().(*types.Signature)
-					params := sig.Params()
-					ptrParams := getNilableParams(params)
-					if len(ptrParams) > 0 {
-						unsanitized := runBlk(pass, fnDecl.Body, ptrParams)
+					interestingParams := getNilableParams(sig.Params())
+					namedTyps := aggregateNamedTyps(pass)
+					typCollection := getAllInnerTyps(pass, nil, interestingParams, namedTyps)
+					// TODO: Remove me
+					// for _, v := range typCollection {
+					// 	for _, vv := range v {
+					// 		fmt.Println(vv)
+					// 	}
+					// }
+
+					if len(interestingParams) > 0 {
+						ctx := NewContext(pass, interestingParams, typCollection)
+						unsanitized := runBlk(ctx, fnDecl.Body)
 						addReports(pass, fn, unsanitized)
-						if TestOn {
-							for _, violated := range unsanitized {
-								pass.Reportf(violated.DeclaredAt, fmt.Sprintf("Declared '%s'", violated.Param.Name()))
-								pass.Reportf(violated.UseAt.Pos(), fmt.Sprintf("Unsafely used '%s'", violated.Param.Name()))
-							}
-						}
+						reportOnTest(pass, unsanitized)
 					}
 				}
 			}
@@ -69,19 +159,19 @@ func run(pass *analysis.Pass) (interface{}, error) {
 	return nil, nil
 }
 
-func getNilableParams(params *types.Tuple) []*types.Var {
-	var ptrParams []*types.Var
+func getNilableParams(params *types.Tuple) []types.Object {
+	var ptrParams []types.Object
 	for i := 0; i < params.Len(); i++ {
 		p := params.At(i)
 		switch p.Type().Underlying().(type) {
-		case *types.Pointer, *types.Slice, *types.Map, *types.Interface, *types.Signature:
+		case *types.Pointer, *types.Slice, *types.Map, *types.Interface, *types.Signature, *types.Struct:
 			ptrParams = append(ptrParams, p)
 		}
 	}
 	return ptrParams
 }
 
-func runBlk(pass *analysis.Pass, blkStmt *ast.BlockStmt, params []*types.Var) []*ParamUsage {
+func runBlk(ctx Context, blkStmt *ast.BlockStmt) []*ParamUsage {
 	var (
 		uses         []*ParamUsage
 		guards       []*ParamUsage
@@ -90,23 +180,34 @@ func runBlk(pass *analysis.Pass, blkStmt *ast.BlockStmt, params []*types.Var) []
 	)
 	if blkStmt != nil {
 		ast.Inspect(blkStmt, func(n ast.Node) bool {
-			paramUsage := runExpr(pass, n, params)
-			if paramUsage != nil {
-				if paramUsage.GuardAt != nil {
-					guards = append(guards, paramUsage)
-				}
-				if paramUsage.UseAt != nil {
-					uses = append(uses, paramUsage)
+			paramUsages := runExpr(ctx, n)
+			for _, usage := range paramUsages {
+				if usage != nil {
+					if usage.guardAt != nil {
+						guards = append(guards, usage)
+					}
+					if usage.useAt != nil {
+						uses = append(uses, usage)
+					}
+
 				}
 			}
+			// TODO: Reconsider me
 			return true
+			if len(paramUsages) > 0 {
+				// if a violated point of expression is found
+				// do not recursively iterate its children (to avoid duplicated report)
+				return false
+			} else {
+				return true
+			}
 		})
 	}
 
 	for _, guard := range guards {
 		for _, use := range uses {
-			if guard.Param.Id() == use.Param.Id() &&
-				guard.GuardAt.Pos() < use.UseAt.Pos() {
+			if guard.param.Id() == use.param.Id() &&
+				guard.guardAt.Pos() <= use.useAt.Pos() {
 				// violated: guard expression must appear before the use expression
 				sanitizedUse[use] = true
 			}
@@ -127,43 +228,73 @@ func runBlk(pass *analysis.Pass, blkStmt *ast.BlockStmt, params []*types.Var) []
 	return unsanitized
 }
 
-func runExpr(pass *analysis.Pass, n ast.Node, params []*types.Var) *ParamUsage {
+func runExpr(ctx Context, n ast.Node) []*ParamUsage {
 	switch expr := n.(type) {
 	case *ast.CallExpr:
-		if fnIdent := match(pass, expr.Fun, params); fnIdent != nil {
-			return NewParamUsage(fnIdent, nil, expr, fnIdent.Pos())
+		if fnIdent := match(ctx.pass, expr.Fun, ctx.params); fnIdent != nil {
+			return []*ParamUsage{NewParamUsage(fnIdent, nil, expr, fnIdent.Pos())}
 		}
 	case *ast.BinaryExpr:
-		return runBinaryExpr(pass, expr, params)
-
+		return runBinaryExpr(ctx, expr)
 	case *ast.TypeSwitchStmt:
-		return runTypSwitchStmt(pass, expr, params)
+		return []*ParamUsage{runTypSwitchStmt(ctx, expr)}
 	case *ast.StarExpr:
-		if v := match(pass, expr, params); v != nil {
-			return NewParamUsage(v, nil, expr, v.Pos())
+		children := getSelectorExprChildren(expr.X)
+		// depth n: e.g., *s.member1.member2
+		if len(children) > 0 {
+			mostParentExpr := children[len(children)-1].X
+			lastChildIdent := children[0].Sel
+			if v := match(ctx.pass, mostParentExpr, ctx.params); v != nil && v.Type().String() != "*testing.T" {
+				for _, innerChildren := range ctx.typCollection {
+					for ident, typ := range innerChildren {
+						if ident.Name == lastChildIdent.Name {
+							if _, ok := typ.Type().Underlying().(*types.Pointer); ok {
+								usage := ctx.pass.TypesInfo.Uses[lastChildIdent]
+								paramUsage := NewParamUsage(usage, nil, expr, v.Pos())
+								paramUsage.context = v
+								return []*ParamUsage{paramUsage}
+							}
+						}
+					}
+				}
+			}
 		}
-	case *ast.SelectorExpr:
-		if v := match(pass, expr.X, params); v != nil {
-			if v.Type().String() != "*testing.T" {
-				return NewParamUsage(v, nil, expr, v.Pos())
+
+		// depth 1: e.g., *v
+		if v := match(ctx.pass, expr, ctx.params); v != nil {
+			return []*ParamUsage{NewParamUsage(v, nil, expr, v.Pos())}
+		}
+	case *ast.SelectorExpr: // rule: if a struct is pointer && access its member
+		// depth n: e.g., s.member1.member2
+		if paramUsage := runSelectorExprTree(ctx, expr, true); paramUsage != nil {
+			return paramUsage
+		}
+
+		// depth 1: e.g., s.member
+		if v := match(ctx.pass, expr.X, ctx.params); v != nil {
+			switch v.Type().Underlying().(type) {
+			case *types.Pointer, *types.Interface:
+				if v.Type().String() != "*testing.T" {
+					return []*ParamUsage{NewParamUsage(v, nil, expr, v.Pos())}
+				}
 			}
 		}
 	case *ast.SliceExpr:
-		if v := match(pass, expr.X, params); v != nil {
-			return NewParamUsage(v, nil, expr, v.Pos())
+		if v := match(ctx.pass, expr.X, ctx.params); v != nil {
+			return []*ParamUsage{NewParamUsage(v, nil, expr, v.Pos())}
 		}
 	case *ast.IndexExpr:
-		if v := match(pass, expr.X, params); v != nil {
-			return NewParamUsage(v, nil, expr, v.Pos())
+		if v := match(ctx.pass, expr.X, ctx.params); v != nil {
+			return []*ParamUsage{NewParamUsage(v, nil, expr, v.Pos())}
 		}
 	}
 	return nil
 }
 
-func lenCompGuard(pass *analysis.Pass, expr ast.Expr, params []*types.Var) *ParamUsage {
+func lenCompGuard(ctx Context, expr ast.Expr) *ParamUsage {
 	if callExpr, isCallExpr := expr.(*ast.CallExpr); isCallExpr {
 		if fnIdent := cast2Ident(callExpr); fnIdent != nil && fnIdent.Name == "len" {
-			if lenParam := match(pass, callExpr.Args[0], params); lenParam != nil {
+			if lenParam := match(ctx.pass, callExpr.Args[0], ctx.params); lenParam != nil {
 				return NewParamUsage(lenParam, expr, nil, lenParam.Pos())
 			}
 		}
@@ -171,45 +302,59 @@ func lenCompGuard(pass *analysis.Pass, expr ast.Expr, params []*types.Var) *Para
 	return nil
 }
 
-func runBinaryExpr(pass *analysis.Pass, binaryExpr *ast.BinaryExpr, params []*types.Var) *ParamUsage {
+func runBinaryExpr(ctx Context, binaryExpr *ast.BinaryExpr) []*ParamUsage {
 	// Guard Definition 1
 	lhs, op, rhs := binaryExpr.X, binaryExpr.Op, binaryExpr.Y
-	lhsV := match(pass, lhs, params)
-	rhsV := match(pass, rhs, params)
+
+	if paramUsages := runSelectorExprTree(ctx, lhs, false); paramUsages != nil {
+		for _, usage := range paramUsages {
+			usage.guardAt = binaryExpr
+		}
+		return paramUsages
+	}
+	if paramUsages := runSelectorExprTree(ctx, rhs, false); paramUsages != nil {
+		for _, usage := range paramUsages {
+			usage.guardAt = binaryExpr
+		}
+		return paramUsages
+	}
+
+	lhsV := match(ctx.pass, lhs, ctx.params)
+	rhsV := match(ctx.pass, rhs, ctx.params)
 	if op == token.EQL || op == token.NEQ {
 		if lhsV != nil {
 			if rhsIdent := cast2Ident(rhs); rhsIdent != nil && rhsIdent.Name == "nil" {
-				return NewParamUsage(lhsV, binaryExpr, nil, lhsV.Pos())
+				return []*ParamUsage{NewParamUsage(lhsV, binaryExpr, nil, lhsV.Pos())}
 			}
 		}
 		if rhsV != nil {
 			if lhsIdent := cast2Ident(lhs); lhsIdent != nil && lhsIdent.Name == "nil" {
-				return NewParamUsage(rhsV, binaryExpr, nil, rhsV.Pos())
+				return []*ParamUsage{NewParamUsage(rhsV, binaryExpr, nil, rhsV.Pos())}
 			}
 		}
 	}
 
 	// Guard Definition 2
 	if op == token.EQL || op == token.NEQ || op == token.LEQ || op == token.GEQ || op == token.LSS || op == token.GTR {
-		if lenParamUsage := lenCompGuard(pass, lhs, params); lenParamUsage != nil {
-			if isSliceTyp(lenParamUsage.Param) {
-				return lenParamUsage
+		if lenParamUsage := lenCompGuard(ctx, lhs); lenParamUsage != nil {
+			if isSliceTyp(lenParamUsage.param) {
+				return []*ParamUsage{lenParamUsage}
 			}
 		}
-		if lenParamUsage := lenCompGuard(pass, rhs, params); lenParamUsage != nil {
-			if isSliceTyp(lenParamUsage.Param) {
-				return lenParamUsage
+		if lenParamUsage := lenCompGuard(ctx, rhs); lenParamUsage != nil {
+			if isSliceTyp(lenParamUsage.param) {
+				return []*ParamUsage{lenParamUsage}
 			}
 		}
 	}
 	return nil
 }
 
-func runTypSwitchStmt(pass *analysis.Pass, typSwitchStmt *ast.TypeSwitchStmt, params []*types.Var) *ParamUsage {
+func runTypSwitchStmt(ctx Context, typSwitchStmt *ast.TypeSwitchStmt) *ParamUsage {
 	// Guard Definition 3
 	if assignExpr, ok := typSwitchStmt.Assign.(*ast.ExprStmt); ok {
 		if typAssertExpr, ok := assignExpr.X.(*ast.TypeAssertExpr); ok {
-			if v := match(pass, typAssertExpr.X, params); v != nil {
+			if v := match(ctx.pass, typAssertExpr.X, ctx.params); v != nil {
 				if _, ok := v.Type().Underlying().(*types.Interface); ok {
 					return NewParamUsage(v, typSwitchStmt, nil, v.Pos())
 				}
@@ -219,7 +364,24 @@ func runTypSwitchStmt(pass *analysis.Pass, typSwitchStmt *ast.TypeSwitchStmt, pa
 	return nil
 }
 
-func isSliceTyp(paramTyp *types.Var) bool {
+func getSelectorExprChildren(expr ast.Expr) []*ast.SelectorExpr {
+	e, ok := expr.(*ast.SelectorExpr)
+	if !ok {
+		return nil
+	}
+	children := []*ast.SelectorExpr{e}
+	for {
+		if child, ok := e.X.(*ast.SelectorExpr); ok {
+			e = child
+			children = append(children, child)
+		} else {
+			break
+		}
+	}
+	return children
+}
+
+func isSliceTyp(paramTyp types.Object) bool {
 	_, isSliceTyp := paramTyp.Type().Underlying().(*types.Slice)
 	return isSliceTyp
 }
@@ -237,10 +399,66 @@ func cast2Ident(expr ast.Expr) *ast.Ident {
 	}
 }
 
-func match(pass *analysis.Pass, expr ast.Expr, params []*types.Var) *types.Var {
+func runSelectorExprTree(ctx Context, expr ast.Expr, use bool) []*ParamUsage {
+	children := getSelectorExprChildren(expr)
+	if len(children) == 0 {
+		return nil
+	}
+
+	mostParentExpr := children[len(children)-1].X
+	lastChildIdent := children[0].Sel
+
+	var usages []*ParamUsage
+	if v := match(ctx.pass, mostParentExpr, ctx.params); v != nil && v.Type().String() != "*testing.T" {
+		mostParentIdent := cast2Ident(mostParentExpr)
+		mostParentTyp := ctx.pass.TypesInfo.Uses[mostParentIdent].Type()
+		for typ, innerChildren := range ctx.typCollection {
+			if typ == mostParentTyp {
+				for ident, typ := range innerChildren {
+					switch typ.Type().Underlying().(type) {
+					case *types.Pointer, *types.Interface:
+						start := 0
+						if use {
+							// Skip the first child, which is the last property (e.g., a.b.c.last)
+							start = 1
+						}
+						for i := start; i < len(children); i++ {
+							if children[i].Sel.Name == ident.Name {
+								usage := ctx.pass.TypesInfo.Uses[children[i].Sel]
+								if usage.Type() == typ.Type() {
+									if use {
+										paramUsage := NewParamUsage(usage, nil, expr, v.Pos())
+										paramUsage.context = v
+										usages = append(usages, paramUsage)
+									} else {
+										if lastChildIdent.Name == ident.Name {
+											paramUsage := NewParamUsage(usage, expr, nil, v.Pos())
+											paramUsage.context = v
+											usages = append(usages, paramUsage)
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	return usages
+}
+
+func match(pass *analysis.Pass, expr ast.Expr, params []types.Object) types.Object {
 	obj := pass.TypesInfo.ObjectOf(cast2Ident(expr))
+	if obj != nil {
+		return findParam(obj.Type(), params)
+	}
+	return nil
+}
+
+func findParam(objTyp types.Type, params []types.Object) types.Object {
 	for _, param := range params {
-		if obj == param {
+		if objTyp == param.Type() {
 			return param
 		}
 	}
--- pass/report.go
+++ pass/report.go
@@ -16,7 +16,7 @@ type ReportMsg struct {
 }
 
 var (
-	GlobalRWMutex = new(sync.RWMutex)
+	ReportRWMutex = new(sync.RWMutex)
 	Reports       = make(map[token.Pos][]ReportMsg)
 	ReportIdx     = 0
 )
@@ -27,34 +27,38 @@ func newReportMsg(pos token.Pos, msg string) ReportMsg {
 
 func addReports(pass *analysis.Pass, fnDecl types.Object, violatedUses []*ParamUsage) {
 	for _, violatedUse := range violatedUses {
-		declPos, usePos := violatedUse.DeclaredAt, violatedUse.UseAt.Pos()
+		declPos, usePos := violatedUse.declaredAt, violatedUse.useAt.Pos()
 		declLoc, useLoc := pass.Fset.Position(declPos), pass.Fset.Position(usePos)
 
-		paramName := violatedUse.Param.Name()
+		paramName := violatedUse.param.Name()
+		violatedAtContext := ""
+		if violatedUse.context != nil {
+			violatedAtContext = fmt.Sprintf("(member: '%s')", violatedUse.param.Name())
+			paramName = violatedUse.context.Name()
+		}
 
 		idx := color.New(color.FgRed).Sprintf("%4d", ReportIdx)
 		declMsg := fmt.Sprintf("[%s] Declared '%s' at -> %s", idx, paramName, declLoc)
-		// prefix := color.New(color.Underline).Sprintf("Unsafely used")
 		prefix := "Unsafely used"
-		useMsg := fmt.Sprintf("  --> %s '%s' at -> %s", prefix, paramName, useLoc)
+		useMsg := fmt.Sprintf("  --> %s '%s' %s at -> %s", prefix, paramName, violatedAtContext, useLoc)
 
-		GlobalRWMutex.Lock()
+		ReportRWMutex.Lock()
 		if len(Reports[declPos]) == 0 {
 			Reports[declPos] = append(Reports[declPos], newReportMsg(declPos, declMsg))
 			ReportIdx++
 		}
 		Reports[declPos] = append(Reports[declPos], newReportMsg(usePos, useMsg))
-		GlobalRWMutex.Unlock()
+		ReportRWMutex.Unlock()
 	}
 }
 
 func printReports(pass *analysis.Pass) {
-	GlobalRWMutex.RLock()
-	defer GlobalRWMutex.RUnlock()
+	ReportRWMutex.RLock()
+	defer ReportRWMutex.RUnlock()
 
 	for _, reportByDecl := range Reports {
 		for _, report := range reportByDecl {
-			if !TestOn {
+			if !Testing.on {
 				pass.Reportf(0, report.msg)
 			}
 		}
